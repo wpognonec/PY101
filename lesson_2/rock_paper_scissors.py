46be8761f24b5f6ca6799b69a29f6abe25e33b8f
William Pognonec <william.pognonec@gmail.com>
2024-02-15 10:37:58 -0600

    Dynamically build title, refactor some code, add json file for large strings / objects

--- lesson_2/rock_paper_scissors.py
+++ lesson_2/rock_paper_scissors.py
@@ -1,32 +1,23 @@
 import random
 import os
+import json
 
-CHOICES = {
-    "r": "rock",
-    "p": "paper",
-    "s": "scissors",
-    "l": "lizard",
-    "sp": "spock",
-}
+with open('rps_strings.json', 'r') as file:
+    DATA = json.load(file)
 
-WIN_COMBO = {
-    "rock": ["scissors", "lizard"],
-    "paper": ["rock", "spock"],
-    "scissors": ["paper", "lizard"],
-    "lizard": ["paper", "spock"],
-    "spock": ["rock", "scissors"],
-}
-
+CHOICES = DATA["choices"]
+WIN_COMBO = DATA["win_combos"]
 CLEAR = "cls" if os.name == "nt" else "clear"
 
 def prompt(msg):
     print(f"==> {msg}")
 
-def get_choice():
+def input_choice():
     while True:
         prompt(
             "Choose one: "
-            + ", ".join([format_choice(item) for item in CHOICES.values()])
+            + ", ".join([format_choice(item, parens=True)
+                         for item in CHOICES.values()])
         )
         choice = input("==> ").lower()
 
@@ -35,13 +26,28 @@ def get_choice():
         if choice in CHOICES.values():
             return choice
         os.system(CLEAR)
-        prompt(f"{choice} is not a valid choice.")
+        prompt(
+            f"{choice} is not a valid choice." if choice
+            else "You must enter a choice"
+        )
 
-def format_choice(choice):
-    if choice == "spock":
+def input_yes_no():
+    while True:
+        choice = input("==> ").lower()
+        if choice not in ["y", "n"]:
+            os.system(CLEAR)
+            prompt("Please enter 'y' or 'n'")
+        else:
+            break
+    return choice
+
+def format_choice(choice, parens=False):
+    if parens and choice == "spock":
         formatted_choice = "(Sp)ock"
-    else:
+    elif parens:
         formatted_choice = f"({choice[0].upper()}){choice[1:]}"
+    else:
+        formatted_choice = f"{choice[0].upper()+choice[1:]}"
     return formatted_choice
 
 def get_winner(choice, computer_choice):
@@ -56,9 +62,28 @@ def display_winner(winner):
         case "player":
             prompt("You win!")
         case "computer":
-            prompt("You loose!")
+            prompt("You lose!")
         case "tie":
             prompt("It's a tie!")
+
+def display_score(scores):
+    prompt(
+        'The current score is '
+        f'player: {scores["player"]} - computer: {scores["computer"]}'
+    )
+    if scores["player"] == 3:
+        prompt("You have won the game!")
+    elif scores["computer"] == 3:
+        prompt("The computer has won the game!")
+
+def display_choices(choice, computer_choice):
+    prompt(
+        f"You chose {format_choice(choice)}, "
+        f"computer chooses {format_choice(computer_choice)}"
+    )
+
+def display_rules():
+    prompt(DATA["rules"])
 
 def update_scores(winner, scores):
     match winner:
@@ -67,64 +92,42 @@ def update_scores(winner, scores):
         case "computer":
             scores["computer"] += 1
 
-def display_score(scores):
-    prompt(f'The current score is {scores["player"]}-{scores["computer"]}')
-    if scores["player"] == 3:
-        prompt("You have won the game!")
-    elif scores["computer"] == 3:
-        prompt("The computer has won the game!")
-
-def get_yes_no():
-    while True:
-        choice = input("==> ").lower()
-        if choice not in ["y", "n"]:
-            os.system(CLEAR)
-            prompt("Please enter 'y' or 'n'")
-        else:
-            break
-    return choice
-
-def display_rules():
-    prompt('''Best of 3 wins.
-    - Scissors cuts Paper
-    - Paper covers Rock
-    - Rock crushes Lizard
-    - Lizard poisons Spock
-    - Spock smashes Scissors
-    - Scissors decapitates Lizard
-    - Lizard eats Paper
-    - Paper disproves Spock
-    - Spock vaporizes Rock
-    - Rock crushes Scissors
-    ''')
+def play_again():
+    prompt("Would you like to play again? (y/n):")
+    choice = input_yes_no()
+    if choice[0] == "n":
+        prompt("Thank you for playing!")
+        return False
+    return True
 
 def intro():
     os.system(CLEAR)
-    prompt("Welcome to Rock Paper Scissors Lizard Spock!")
+    prompt(f"Welcome to {" ".join([format_choice(item)
+                                   for item in CHOICES.values()])}!")
     prompt("Would you like to see the rules? (y/n)")
-    choice = get_yes_no()
+    choice = input_yes_no()
     if choice[0] == "y":
         display_rules()
-    play()
 
 def play():
+    intro()
     while True:
         scores = {"player": 0, "computer": 0}
         while scores["player"] != 3 and scores["computer"] != 3:
-            choice = get_choice()
+            prompt("Best of 3 wins!")
+            choice = input_choice()
             computer_choice = random.choice(list(CHOICES.values()))
 
             os.system(CLEAR)
-            prompt(f"You chose {choice}, computer chooses {computer_choice}")
+            display_choices(choice, computer_choice)
             winner = get_winner(choice, computer_choice)
             display_winner(winner)
             update_scores(winner, scores)
             display_score(scores)
 
-        prompt("Would you like to play again? (y/n):")
-        choice = get_yes_no()
-        if choice[0] == "n":
+        if not play_again():
             break
+
         os.system(CLEAR)
 
-intro()
+play()
